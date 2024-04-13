5de68ff2705e679aadc2dd005ed65d76d815f7aa
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-13 18:25:38 +0530

    add: model defn

--- backend/parking_slot/models.py
+++ backend/parking_slot/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class ParkingSlot(models.Model):
+    
+    class ParkingSlotStatusChoices(models.TextChoices):
+        OCCUPIED = "Occupied"
+        VACANT = "Vacant"
+        
+    slot_id = models.AutoField(primary_key=True)
+    location = models.CharField(max_length=100)
+    status = models.CharField(default=ParkingSlotStatusChoices.VACANT ,max_length=10, choices=ParkingSlotStatusChoices.choices)
+    expiration = models.DateTimeField()
