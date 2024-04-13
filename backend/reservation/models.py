48a42c8f8e49f42ee3338a4e907c3af48480e26c
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-13 18:41:18 +0530

    add: models

--- backend/reservation/models.py
+++ backend/reservation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from parking_slot.models import ParkingSlot
 
-# Create your models here.
+class Reservation(models.Model):
+    
+    class ReservationStatusChoices(models.TextChoices):
+        ACTIVE = "ACTIVE"
+        CANCELLED = "CANCELLED"
+    
+    reservation_id = models.AutoField(primary_key=True)
+    slot = models.ForeignKey(ParkingSlot, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(default=ReservationStatusChoices.ACTIVE, max_length=20, choices=ReservationStatusChoices.choices)
