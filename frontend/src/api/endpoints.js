2fcfb401f41653c24f2254d44ae2d0a31cdc3fd7
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-14 18:28:36 +0530

    add: endpoints

---
+++ frontend/src/api/endpoints.js
@@ -0,0 +1,13 @@
+function ParkingSlots() {
+    const GET_ALL_PARKING_SLOTS = "parking_slots/all/";
+}
+
+function Reservations() {
+
+}
+
+function Transactions() {
+
+}
+
+export {ParkingSlots, Reservations, Transactions};
