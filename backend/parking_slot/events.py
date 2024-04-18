fed4e1f1e81e62274103eb13df84675b87788ba8
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-18 10:32:10 +0530

    add: cron job

---
+++ backend/parking_slot/events.py
@@ -0,0 +1,15 @@
+# management/commands/check_slots.py
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from parking_slot.models import ParkingSlot
+
+class Command(BaseCommand):
+    help = 'Check parking slots for expiration and update their status'
+
+    def handle(self, *args, **options):
+        current_time = timezone.now()
+        expired_slots = ParkingSlot.objects.filter(status=ParkingSlot.ParkingSlotStatusChoices.OCCUPIED, expiration__lte=current_time)
+        for slot in expired_slots:
+            slot.status = ParkingSlot.ParkingSlotStatusChoices.VACANT
+            slot.expiration = None
+            slot.save()
