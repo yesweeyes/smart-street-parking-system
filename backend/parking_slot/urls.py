ebb6442d29c0504932d817ac76d43d919347ab47
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-18 21:32:38 +0530

    fix: parking slot urls

--- backend/parking_slot/urls.py
+++ backend/parking_slot/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from parking_slot.views import ParkingSlotAPIView, ParkingSlotCreateAPIView, ParkingSlotDetailAPIView
 
 urlpatterns = [
-    path("", ParkingSlotCreateAPIView.as_view(), name="parking-slot-create"),
-    path("all/", ParkingSlotAPIView.as_view(), name="parking-slots-all"),
+    path("", ParkingSlotAPIView.as_view(), name="parking-slots-all"),
     path("<int:id>/", ParkingSlotDetailAPIView.as_view(), name="parking-slot-detail")
 ]
