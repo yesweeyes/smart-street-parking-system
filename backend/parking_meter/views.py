f293b2a48ad9e91bacfbafcbbd9fe884b6e00bc3
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-18 21:33:03 +0530

    add: parking meter api

--- backend/parking_meter/views.py
+++ backend/parking_meter/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from parking_meter.models import ParkingMeter
+from parking_meter.serializer import ParkingMeterSerializer
 
-# Create your views here.
+class ParkingMeterAPIView(APIView):
+    def get(self, request):
+        parking_meters = ParkingMeter.objects.all()
+        serializer = ParkingMeterSerializer(parking_meters, many=True)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def post(self, request):
+        serializer = ParkingMeterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ParkingMeterDetailAPIView(APIView):
+    def get(self, request, id):
+        parking_meter = ParkingMeter.objects.get(meter_id=id)
+    
+    def delete(self, request, id):
+        pass
