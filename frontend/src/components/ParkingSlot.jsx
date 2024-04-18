8af3f9b680951489f9e787989f06e36a760beeb2
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-18 10:30:17 +0530

    add: home page

--- frontend/src/components/ParkingSlot.jsx
+++ frontend/src/components/ParkingSlot.jsx
@@ -1,5 +1,49 @@
-function ParkingSlot(props) {
+import React, { useState } from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Paper, Typography } from '@mui/material';
 
+function ParkingSlot({ parkingSlot }) {
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleSlotClick = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const parkingSlotStatusColors = {
+    OCCUPIED: '#F44336',
+    VACANT: '#4CAF50',
+  };
+
+  return (
+    <div className="parking-grid">
+        <Button onClick={handleSlotClick} size="small">
+          <Paper elevation={3} sx={{ padding: 2, height: '150px', width: '100px', overflow: 'hidden', backgroundColor: parkingSlotStatusColors[parkingSlot.status] }}>
+            <Typography variant="subtitle1">Slot ID: {parkingSlot.slot_id}</Typography>
+            <Typography variant="body2">Status: {parkingSlot.status}</Typography>
+          </Paper>
+        </Button>
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogTitle>Slot Details</DialogTitle>
+        <DialogContent dividers>
+          <Typography variant="body1">Slot ID: {parkingSlot.slot_id}</Typography>
+          <Typography variant="body1">Status: {parkingSlot.status}</Typography>
+          <Typography variant="body1">Location: {parkingSlot.location}</Typography>
+          <Typography variant="body1">Expiration: {parkingSlot.expiration?  
+          new Date(parkingSlot.expiration).toLocaleString("en-US", { dateStyle: "medium", timeStyle: "short" })
+          : "None"}
+          </Typography>
+          <Typography variant="body1">Battery Level: {parkingSlot.parkingmeter.battery_level}</Typography>
+          <Typography variant="body1">Meter Status: {parkingSlot.parkingmeter.status}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal} color="primary" size="small">Close</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 export default ParkingSlot;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,30 @@
-function HomePage() {
+import React, { useState, useEffect } from "react";
+import { Grid } from "@mui/material";
+import ParkingSlot from "../components/ParkingSlot";
+import { ParkingSlots as ParkingSlotEndpoint } from "../api/endpoints";
+import api from "../api/api";
 
+function HomePage() {
+  const [parkingSlots, setParkingSlots] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("slot/all/")
+      .then((response) => setParkingSlots(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <>
+    <Grid container spacing={2}>
+      {parkingSlots.map((slot) => (
+        <Grid item key={slot.slot_id} xs={1.2} sm={1.2} md={1.2} lg={1.2}>
+          <ParkingSlot parkingSlot={slot} />
+        </Grid>
+      ))}
+    </Grid>
+    </>
+  );
 }
 
 export default HomePage;
