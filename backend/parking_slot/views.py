d494deff75474ba081d224b9bed66eaa49a6331e
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-18 18:53:01 +0530

    add: delete slot api

--- backend/parking_slot/views.py
+++ backend/parking_slot/views.py
@@ -3,9 +3,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from parking_slot.models import ParkingSlot
 from parking_slot.serializer import ParkingSlotSerializer
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class ParkingSlotAPIView(APIView):
     def get(self, request):
         parking_slots_with_meter = ParkingSlot.objects.select_related("parkingmeter")
         serializer = ParkingSlotSerializer(parking_slots_with_meter, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class ParkingSlotDetailAPIView(APIView):
+    def delete(self, id:int):
+        parking_slot = get_object_or_404(ParkingSlot, id)
+        parking_slot.delete()
+        
+        return Response({"data":"Parking slot has been deleted"}, status=status.HTTP_200_OK)
