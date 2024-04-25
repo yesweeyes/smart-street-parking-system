4ea326450d217648b39a5e7dcfe6282f7160a217
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-25 12:10:26 +0530

    fix: hardcode choreo url

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL? 
+    import.meta.env.VITE_API_URL:
+    "/choreo-apis/smart-parking-system/smartparkingsystembackend/rest-api-be2/v1.0"
 });
 
 export default api;
