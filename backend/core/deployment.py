ef3aae2ce62da901239463630af9dd402a1b631a
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-25 19:24:14 +0530

    fix: settings file

--- backend/core/deployment.py
+++ backend/core/deployment.py
@@ -39,12 +39,13 @@ CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('-')[0]:pair.split('-')[1] for pair in CONNECTION.split(' ')}
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": CONNECTION_STR['dbname'],
-        "HOST": CONNECTION_STR['host'],
-        "USER": CONNECTION_STR['user'],
-        "PASSWORD": CONNECTION_STR['password'],
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':os.getenv('DB_NAME'),
+        'USER':os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST':os.getenv('DB_HOST'),
+        'PORT':'5432',
     }
 }
 
