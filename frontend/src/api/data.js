b5c6c1e3c0a2ae7cd53ecbe4c1b364af17451f95
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-25 23:57:15 +0530

    fix: backend prod endpoint

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const api = axios.create({
     baseURL: import.meta.env.MODE == "LOCAL"? 
     import.meta.env.VITE_API_URL_LOCAL:
-    "/api/v1"
+    import.meta.env.VITE_API_URL_PROD
 });
 
 export default api;
