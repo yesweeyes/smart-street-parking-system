adfde660167f5c81c91d4218ace14ed35a19acfe
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-25 23:33:08 +0530

    add: deploy frontend

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL? 
-    import.meta.env.VITE_API_URL:
-    "/choreo-apis/smart-parking-system/smartparkingsystembackend/rest-api-be2/v1.0"
+    baseURL: import.meta.env.MODE == "PROD"? 
+    import.meta.env.VITE_API_URL_PROD:
+    import.meta.env.VITE_API_URL_LOCAL
 });
 
 export default api;
