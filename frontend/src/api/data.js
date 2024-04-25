986163884716b38dae4d79b55de6ab5c03c96c19
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-26 01:16:14 +0530

    fix: hardcode backend endpoint

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: process.env.MODE == "PROD"? 
-    process.env.VITE_API_URL_PROD:
-    process.env.VITE_API_URL_LOCAL
+    baseURL: "https://smart-parking-system-backend.azurewebsites.net/api/v1/"
 });
 
 export default api;
