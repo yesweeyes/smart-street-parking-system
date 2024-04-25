9135b194ea9c733c00224d52b5c8332f528408b2
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-26 00:28:51 +0530

    fix: point to prod

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.MODE == "LOCAL"? 
+    baseURL: false? 
     import.meta.env.VITE_API_URL_LOCAL:
     import.meta.env.VITE_API_URL_PROD
 });
