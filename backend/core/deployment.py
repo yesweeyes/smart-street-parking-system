52379b0e86ca85f38a993d55df13bb9ec009da16
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-25 23:36:31 +0530

    add: cors allowed origins

--- backend/core/deployment.py
+++ backend/core/deployment.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['*']
 
 load_dotenv()
 
-CSRF_TRUSTED_ORIGINS = ['https://'+os.getenv('WEBSITE_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.getenv('WEBSITE_HOSTNAME'),]
 
 DEBUG = False
 
@@ -23,11 +23,10 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000'
-# ]
-# CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-# CONNECTION_STR = {pair.split('-')[0]:pair.split('-')[1] for pair in CONNECTION.split(' ')}
+CORS_ALLOWED_ORIGINS = [
+   'https://agreeable-mushroom-0f44e6600.5.azurestaticapps.net',
+]
+
 
 DATABASES = {
     'default': {
