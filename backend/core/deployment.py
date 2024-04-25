a509f8ee5c7526f75669af2b8b8c43432f4b1fec
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-26 01:42:14 +0530

    fix: whitenoise static files storage

--- backend/core/deployment.py
+++ backend/core/deployment.py
@@ -31,7 +31,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
