eb4511181ae8f27c8d51c7883abcb18dee1aac23
Shyam Sundar <shyamsundarv00073@gmail.com>
2024-04-26 00:58:42 +0530

    fix: env variables frontend

--- frontend/src/api/data.js
+++ frontend/src/api/data.js
@@ -2,8 +2,8 @@ import axios from "axios"
 
 const api = axios.create({
     baseURL: false? 
-    import.meta.env.VITE_API_URL_LOCAL:
-    import.meta.env.VITE_API_URL_PROD
+    process.env.VITE_API_URL_LOCAL:
+    process.env.VITE_API_URL_PROD
 });
 
 export default api;
